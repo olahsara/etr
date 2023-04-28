d81fae035a180d404a08c618cb45cd7deb3067d7
Vizi Mark <h166218@stud.u-szeged.hu>
2023-04-28 16:59:48 +0200

    $kurzus_id fix

--- role/hallgato/kurzusok/h_lead.php
+++ role/hallgato/kurzusok/h_lead.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 include_once('../../../functions/functions.php');
-
+$kurzus_id = substr($_POST["kurzus_id"],0,-1);
 $ora_id_select = 'SELECT ADATB."Ora"."ORA_ID"
                   FROM ADATB."Kuzus_Ora", ADATB."Hallgato_Ora", ADATB."Hallgato", ADATB."Kurzus", ADATB."Ora"
                   WHERE ADATB."Kuzus_Ora"."ko_Kurzus_id" = ADATB."Kurzus".KURZUS_ID AND ADATB."Kuzus_Ora"."ko_Ora_id" = ADATB."Ora".ORA_ID
                   AND ADATB."Hallgato".HALLGATO_ID = ADATB."Hallgato_Ora"."ho_Hallgato_id" AND ADATB."Ora".ORA_ID = ADATB."Hallgato_Ora"."ho_Ora_id"
-                  AND ADATB."Hallgato".HALLGATO_ID LIKE '.$_SESSION["felhasznalo"]["id"].' AND ADATB."Kurzus".KURZUS_ID LIKE '.$_POST["kurzus_id"];
-
+                  AND ADATB."Hallgato".HALLGATO_ID LIKE '.$_SESSION["felhasznalo"]["id"].' AND ADATB."Kurzus".KURZUS_ID LIKE '.$kurzus_id;
+echo $ora_id_select;
 $ora_params = lekerdez($ora_id_select);
 
 global $ora_id;
@@ -17,7 +17,6 @@ while ($record = oci_fetch_array($ora_params[0], OCI_ASSOC + OCI_RETURN_NULLS)) 
    $ora_id = $record["ORA_ID"];
    echo $ora_id;
 }
-
 close($ora_params[0], $ora_params[1]);
 //header("Location: hiba.php?id=$ora_id");
 //if ( $ora_id === 0 ){
